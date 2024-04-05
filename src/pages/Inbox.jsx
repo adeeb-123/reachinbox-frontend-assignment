a98d067eeea109619f628ee50f735ca03d723a3e
Adeeb Ahmad <adeebsiddiqui77@gmail.com>
2024-04-05 17:22:39 +0530

    Updated Email Section

--- src/pages/Inbox.jsx
+++ src/pages/Inbox.jsx
@@ -3,13 +3,15 @@ import SideNavbar from '../components/global/SideNavbar'
 import Navbar from '../components/global/Navbar'
 import AllInbox from '../components/Openbox/AllInbox'
 import MailDetails from '../components/Openbox/MailDetails'
+import EmailSection from '../components/Openbox/EmailSection'
 
 const Inbox = () => {
     return (
         <SideNavbar>
             <Navbar />
-            <div className='w-[100%] flex flex-row justify-between'>
+            <div className='w-[100%] min-h-screen flex flex-row justify-between'>
                 <AllInbox />
+                <EmailSection />
                 <MailDetails />
             </div>
         </SideNavbar>
